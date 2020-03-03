ff6931b66a60f4d8e6bf163ae839a9f601f00d2f
IslamAAli <islam.a.mustafa@gmail.com>
2020-03-02 23:53:15 -0700

    new problems

---
+++ HR - Problems/[HR]_Cycle Detection.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the has_cycle function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool has_cycle(SinglyLinkedListNode* head) {
+    vector<SinglyLinkedListNode*> ptrs; 
+
+    SinglyLinkedListNode* itr = head;
+    while(itr != NULL)
+    {
+        vector<SinglyLinkedListNode*>::iterator v_itr;
+        v_itr = find (ptrs.begin(), ptrs.end(), itr);
+        
+        if (v_itr != ptrs.end())
+            return true;
+        else
+            ptrs.push_back(itr);
+
+        itr = itr->next;
+    }
+
+    return false;
+}
+
+int main()
